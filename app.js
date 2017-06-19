2907a7fba1ee16c7e656fc73c6182384f8e00bd4
Karunya Sethuraman <karunyas@mit.edu>
2017-06-19 15:08:22 -0400

    made a minor change to server file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-
+//add a comment
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
